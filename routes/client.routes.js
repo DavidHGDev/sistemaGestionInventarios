0a4a8a44dc62b353513c8b63229dd704c1c11af6
David H <davyd2h@gmail.com>
2026-04-04 04:49:48 -0500

    creando el middleware que verifica los roles  autorizados en cada ruta

--- routes/client.routes.js
+++ routes/client.routes.js
@@ -1,9 +1,14 @@
 import { getAllClients, getOneClients, createClient, updateClient, deleteClient } from "../controllers/client.controllers.js";
 import e from "express";
+import { verificarToken } from '../middlewares/auth.middleware.js';
+import { verficarRoles } from '../middlewares/verificarRoles.js';
 
 const router = e.Router();
 
-router.get('/', getAllClients);
+router.get('/', 
+    verificarToken, 
+    verficarRoles(['VENDEDOR', 'AUDITOR']), 
+    getAllClients);
 router.get('/:id', getOneClients);
 router.patch('/:id', updateClient);
 router.post('/', createClient);
--- services/auth.services.js
+++ services/auth.services.js
@@ -1,5 +1,6 @@
 import { prisma } from "../lib/prisma.js";
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
 class AuthLogin {
     async login(email, password){
@@ -13,14 +14,31 @@ class AuthLogin {
         if(!passwordCorrecto) {
             throw new Error("Credenciales inválidas");
         }
-        console.log(passwordCorrecto);
+        
+        //payload información publica que va en el token
+        const payload = {
+            id: usuarioExistente.id,
+            role: usuarioExistente.role,
+            name: usuarioExistente.name
+        }
+        //firmar el token 
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: '8h'}
+        )
 
         return {
+            usuario: {
             id: usuarioExistente.id,
             name: usuarioExistente.name,
             email: usuarioExistente.email,
             role: usuarioExistente.role
+            },
+            token: token
+
         }
+
     }
 }
 
