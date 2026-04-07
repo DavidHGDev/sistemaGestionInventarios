3df423a2e6cc9ba8e84ce1572fa8d2a67fa82c45
David H <davyd2h@gmail.com>
2026-04-07 06:32:51 -0500

    se implementa cors par permitir cualquier url para conectar con el backend

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 import e from "express";
 import mainRouter from './routes/index.js'
 import { manejadorDeErrores } from "./middlewares/error.Handler.js";
+import cors from 'cors';
+
+// const corsOption = {
+//     origin: 'http://localhost:5500/',
+//     optionSuccesStatus: 200
+// }
 
 const app = e();
 const PORT = 3007;
 
 app.use(e.json());
+app.use(cors())
 
 app.use('/api', mainRouter); //aquí en app.js, se llama a la ruta principal- 
 
